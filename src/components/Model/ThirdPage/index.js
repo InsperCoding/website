4e8f08753311a310c2a5445873e7abc73a8a2553
AntonioFuziy <antoniovf@al.insper.edu.br>
2021-05-07 17:30:08 -0300

    third page

---
+++ src/components/Model/ThirdPage/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+function ThirdPage() {
+  return (
+    <Container>
+      
+    </Container>
+  );
+}
+
+export default ThirdPage;
